9a77983ba6d7cff989bb26d1982fc2a7bda57989
Samantha Olinsky <67108089+samantha-olinsky@users.noreply.github.com>
2023-03-27 10:40:49 -0400

    displaying search results but problem with constant reloading

--- src/home/ItemPreview.js
+++ src/home/ItemPreview.js
@@ -2,15 +2,15 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const ItemPreview = ({item}, {key}) => {
-  console.log(item.title);
+  console.log(item)
   return(
       <div className="col-xl-3 col-lg-4 col-md-4 col-6 pb-4">
         <div className="card card-primary">
           <img className="card-img-top" src={item.image}/>
           <div className="card-body">
-            <h5 className="card-title">{item.title}</h5>
-            <p className="card-text">{item.otherThing}</p>
-            <Link to={{pathname: "/details", query: item.id}} className="btn btn-primary">See more</Link>
+            <h5 className="card-title">{item.name}</h5>
+            <p className="card-text">{item.address.city}, {item.address.country}</p>
+            <Link to={{pathname: "/details", query: item.xid}} className="btn btn-primary">See more</Link>
           </div>
         </div>
       </div>
--- src/search/index.js
+++ src/search/index.js
@@ -1,22 +1,61 @@
 import {useParams} from "react-router";
+import ItemPreview from "../home/ItemPreview";
+import React from "react";
+import {useState} from "react";
 
-
-
-
-async function fetchSearchResults(query) {
-  const API_KEY = '5ae2e3f221c38a28845f05b61fbd1334eba2a96d5c512d14f5665d87';
-  const response = await fetch(`https://api.opentripmap.com/0.1/en/places/geoname?name=${query}&apikey=${API_KEY}`);
-  const data = await response.json();
-  return data;
-}
+const API_KEY = '5ae2e3f221c38a28845f05b61fbd1334eba2a96d5c512d14f5665d87';
 
 const Search = () => {
   const query = useParams().query;
-  // const data = fetchSearchResults(query).then(data => {return data});
+
+  async function apiGet(method, query) {
+    return await new Promise(function(resolve, reject) {
+      let otmAPI =
+          "https://api.opentripmap.com/0.1/en/places/" +
+          method +
+          "?";
+      if (query !== undefined) {
+        otmAPI += "&" + query;
+      }
+      otmAPI += "&apikey=" + API_KEY;
+      fetch(otmAPI)
+      .then(response => response.json())
+      .then(data => resolve(data))
+      .catch(function(err) {
+        console.log("Fetch Error :-S", err);
+      });
+    });
+  }
+
+  const setLocationResults = (data) => {
+    apiGet("radius", `radius=100&lon=${data.lon}&lat=${data.lat}&limit=100`).then(setPlaces);
+  }
+
+  const setPlaces = (data) => {
+    for (let i = 0; i < data.features.length; i++) {
+      const place = data.features[i];
+      apiGet("xid/" + place.properties.xid).then(setPlaceDetails);
+    }
+  }
+
+  let places = [];
+
+  const setPlaceDetails = (data) => {
+    if (!places.includes(data) && data.image !== undefined) {
+      places.push(data);
+    }
+    setPlaceItems(places.map(place => <ItemPreview item={place} key={place.xid}/>));
+  }
+
+  apiGet("geoname", "name=" + query).then(setLocationResults);
+  const [placeItems, setPlaceItems] = useState(null);
     return (
       <div>
         <h1>Search</h1>
         <h4>Query: {query}</h4>
+        <div className="row col-12 w-100 p-0 m-0">
+          {placeItems}
+        </div>
       </div>
   );
 }
