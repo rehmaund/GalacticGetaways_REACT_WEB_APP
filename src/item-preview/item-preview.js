0d9db857265a2e149a6adf0ad86cd9020a709a99
rehmaund <rehmaund44@gmail.com>
2023-04-11 19:13:00 -0400

    Changed out some of the suggested images if they did not lead to any results. Cleaned up item preview to make them all a standardized size both for the photo and for the card itself.

--- src/item-preview/item-preview.js
+++ src/item-preview/item-preview.js
@@ -1,28 +1,31 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import '../index.css';
 
 const ItemPreview = ({item}, {key}) => {
   return(
       <div className="card border-primary mb-4">
-        <img className="card-img-top" src={item.preview.source}/>
+        <div className="preview-image-container">
+          <img className="card-img-top" src={item.preview.source}/>
+        </div>
         <div className="card-body">
-          <h5 className="card-title">{item.name}</h5>
+          <h5 className="card-title preview-title-overflow">{item.name}</h5>
+          <p className="card-text">{item.address.city}, {item.address.country}</p>
           <div>
-            <Link to={{pathname: "/details", query: item.xid}} className="btn btn-primary float-end">See more</Link>
-            <p className="card-text">{item.address.city}, {item.address.country}</p>
+            <Link to={{pathname: "/details", query: item.xid}} className="btn btn-primary">See more</Link>
           </div>
           <div className="row w-100 pt-3">
             <span className="col-4 text-center">
               <i className="fa fa-heart"/>
-              <span className="ms-2">423</span>
+              <span className="ms-1 preview-font-size">423</span>
             </span>
             <span className="col-4 text-center">
               <i className="fa fa-star"/>
-              <span className="ms-2">272</span>
+              <span className="ms-1 preview-font-size">272</span>
             </span>
             <span className="col-4 text-center">
               <i className="fa fa-comment"/>
-              <span className="ms-2">311</span>
+              <span className="ms-1 preview-font-size">311</span>
             </span>
           </div>
         </div>
