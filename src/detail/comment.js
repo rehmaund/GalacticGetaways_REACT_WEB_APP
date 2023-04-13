099215c18245ef8130d0a97c5549df9bab47231a
Samantha Olinsky <67108089+samantha-olinsky@users.noreply.github.com>
2023-04-13 19:29:36 -0400

    edits to comments

--- src/detail/comment.js
+++ src/detail/comment.js
@@ -3,11 +3,24 @@ import {Link} from "react-router-dom";
 
 const Comment = ({comment}, {key}) => {
   const userType = comment.user.userType;
+  const currentUser = {
+    username: "jack",
+    displayName: "Jack",
+    userType: "ALIEN",
+  }
   return(
-      <div className={`card mb-4 ${userType === "ALIEN" ? "bg-success" : ""} ${userType === "HUMAN" ? "bg-warning" : ""} ${userType === "MODERATOR" ? "bg-danger" : ""}`}>
+      <div className={`card mb-4 bg-light bg-opacity-25`}>
         <div className="card-header">
-          <span className="card-title h5 me-2">{comment.user.displayName}</span>
-          <i className={`fa ${userType === "ALIEN" ? "fa-brands fa-reddit-alien" : ""} ${userType === "HUMAN" ? "fa-solid fa-user-astronaut" : ""} ${userType === "MODERATOR" ? "fa-solid fa-user-shield" : ""}`}/>
+          <Link to={{pathname: `/profile/${comment.user.username}`}}
+              className={`text-decoration-none ${userType === "ALIEN" ? "text-success" : ""} ${userType === "HUMAN" ? "text-warning" : ""} ${userType === "MODERATOR" ? "text-danger" : ""}`}>
+            <span className="card-title lead fw-bold me-2">{comment.user.displayName}</span>
+            <i className={`fa ${userType === "ALIEN" ? "fa-brands fa-reddit-alien" : ""} ${userType === "HUMAN" ? "fa-solid fa-user-astronaut" : ""} ${userType === "MODERATOR" ? "fa-solid fa-user-shield" : ""}`}/>
+          </Link>
+          {(currentUser.userType === "MODERATOR" || comment.user.username === currentUser.username) &&
+            <button className="btn btn-danger float-end">
+              <span className="me-2">Delete</span>
+              <i className="fa-regular fa-trash-can"/>
+            </button>}
         </div>
         <div className="card-body">
           <p className="card-text">{comment.text}</p>
--- src/detail/index.js
+++ src/detail/index.js
@@ -11,16 +11,41 @@ const Detail = () => {
   useEffect(() => {
     dispatch(getPlaceDetailsThunk(xid))
   }, [dispatch, xid])
-  const likes = 423;
-  const recommendations = 272;
-  const comments = 12;
-  const comment = {
+  const numLikes = 423;
+  const numRecommendations = 272;
+  const comment1 = {
+    id: 1,
     placeXid: {xid},
     user: {
-      username: "john1",
-          displayName: "John",
-          userType: "MODERATOR",},
+      username: "john",
+      displayName: "John",
+      userType: "MODERATOR",},
     text: "This is a comment",
+  }
+  const comment2 = {
+    id: 2,
+    placeXid: {xid},
+    user: {
+      username: "jack",
+      displayName: "Jack",
+      userType: "ALIEN",},
+    text: "This is another comment",
+  }
+  const comment3 = {
+    id: 3,
+    placeXid: {xid},
+    user: {
+      username: "jill",
+      displayName: "Jill",
+      userType: "HUMAN",},
+    text: "This is a third comment",
+  }
+  const comments = [comment1, comment2, comment3];
+  const numComments = comments.length;
+  const currentUser = {
+    username: "jack",
+    displayName: "Jack",
+    userType: "ALIEN",
   }
   return (
       <div className="">
@@ -38,25 +63,48 @@ const Detail = () => {
               {place.wikipedia_extracts && <p className="text-secondary">{place.wikipedia_extracts.text}</p>}
               <p className="text-info">Tags: {place.kinds}</p>
               <p className="text-success">Rating: {place.rate}</p>
-              <div className="row w-100 pt-3">
+              <div className="row w-100">
                 <span className="col-4">
                   <i className="fa fa-thumbs-up"/>
-                  <span className="ms-2">{likes}</span>
-                  <span className="ms-1">Likes</span>
+                  <span className="ms-2">{numLikes}</span>
+                  <span className="ms-1">Likes from Aliens</span>
                 </span>
-                <span className="col-4">
+                <span className="col-6">
                   <i className="fa fa-star"/>
-                  <span className="ms-2">{recommendations}</span>
-                  <span className="ms-1">Recommendations</span>
+                  <span className="ms-2">{numRecommendations}</span>
+                  <span className="ms-1">Recommendations from Humans</span>
                 </span>
               </div>
+              {currentUser.userType === "ALIEN" &&
+                  <button className="btn btn-primary mt-2">
+                    <i className="fa fa-thumbs-up"/>
+                    <span className="ms-1">Like</span>
+                  </button>}
+              {currentUser.userType === "HUMAN" &&
+                <button className="btn btn-primary mt-2">
+                  <i className="fa fa-star"/>
+                  <span className="ms-1">Recommend</span>
+                </button>}
             </div>
           </div>
           <hr/>
           <div className="row">
+            <h3>{numComments} Comments</h3>
             <div className="col-6">
-              <h3>{comments} Comments</h3>
-              <Comment comment={comment} key={comment.placeXid + comment.user.username}/>
+              {comments.map(comment => <Comment comment={comment} key={comment.id}/>)}
+            </div>
+            <div className="col-6">
+              <div className="card border-secondary p-3">
+                <h4>Add a new comment!</h4>
+                <form>
+                  <div className="mb-3">
+                    <textarea className="form-control" rows="2" placeholder="What do you want to share?"/>
+                  </div>
+                  <button type="submit" className="btn btn-primary"
+                          // onSubmit={}
+                  >Submit</button>
+                </form>
+              </div>
             </div>
           </div>
         </div>}
