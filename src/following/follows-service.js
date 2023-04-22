4a2c9bf04f63075ecf41bea1edce4ae7e62f4cb9
rehmaund <rehmaund44@gmail.com>
2023-04-21 20:48:56 -0400

    Implementing follows and unfollows capability

--- src/following/follows-service.js
+++ src/following/follows-service.js
@@ -1,31 +1,31 @@
 import axios from "axios";
 
-const USERS_API = "http://localhost:4000/api/users";
+const USERS_API = "http://localhost:4000/api";
 
 const api = axios.create({
     withCredentials: true,
 });
 export const userFollowsUser = async (followerId, followedId) => {
     const response = await api.post(
-        `${USERS_API}/${followerId}/follows/${followedId}`
+        `${USERS_API}/follows/${followerId}/${followedId}`
     );
     return response.data;
 };
 
 export const userUnfollowsUser = async (followerId, followedId) => {
     const response = await api.delete(
-        `${USERS_API}/${followerId}/follows/${followedId}`
+        `${USERS_API}/unfollows/${followerId}/${followedId}`
     );
     return response.data;
 };
 
 export const findFollowsByFollowedId = async (followed) => {
-    const response = await api.get(`${USERS_API}/${followed}/followers`);
+    const response = await api.get(`${USERS_API}/followers/${followed}`);
     return response.data;
 };
 
 export const findFollowsByFollowerId = async (follower) => {
-    const response = await api.get(`${USERS_API}/${follower}/followees`);
+    const response = await api.get(`${USERS_API}/following/${follower}`);
     return response.data;
 };
 
