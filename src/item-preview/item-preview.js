c78a07e50e039924f7f69bb752a586386941efd4
rehmaund <rehmaund44@gmail.com>
2023-04-23 16:00:53 -0400

    Minor formatting changes for preview

--- src/item-preview/item-preview.js
+++ src/item-preview/item-preview.js
@@ -11,9 +11,9 @@ const ItemPreview = ({item}, {key}) => {
         <div className="card-body">
           <h5 className="card-title text-truncate">{item.name}</h5>
           <div>
-              <Link to={{pathname: `/details/${item.xid}`}} className="btn btn-primary float-end mt-1">More</Link>
+              <Link to={{pathname: `/details/${item.xid}`}} className="btn btn-primary float-end mt-1 ml-1">More</Link>
             <p className="card-text text-truncate override-card-text">{item.address.city ? `${item.address.city},`: `${item.address.country}`}</p>
-              <p className="card-text text-truncate">{item.address.city  ? `${item.address.country}` : `''`}</p>
+              <p className="card-text text-truncate">{item.address.city  ? `${item.address.country}` : '\u00A0'}</p>
           </div>
         </div>
       </div>
